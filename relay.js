b0318d80fe872efec2da77b563f3ea78c62b5203
khamkarpiyush-hub <khamkarpiyush061@gmail.com>
2026-08-16 20:55:52 +0530

    Use clean standard circuit relay configuration

--- relay.js
+++ relay.js
@@ -3,6 +3,7 @@ import { webSockets } from '@libp2p/websockets';
 import { noise } from '@chainsafe/libp2p-noise';
 import { yamux } from '@chainsafe/libp2p-yamux';
 import { circuitRelayServer } from '@libp2p/circuit-relay-v2';
+import { gossipsub } from '@chainsafe/libp2p-gossipsub';
 import { identify } from '@libp2p/identify';
 
 async function main() {
@@ -17,11 +18,12 @@ async function main() {
     streamMultiplexers: [yamux()],
     services: {
       relay: circuitRelayServer(),
+      pubsub: gossipsub(),
       identify: identify()
     }
   });
 
-  console.log(`🚀 SwarmVault Public Relay running successfully!`);
+  console.log(`🚀 SwarmVault Public Relay running successfully with PubSub!`);
   console.log(`Peer ID: ${server.peerId.toString()}`);
   server.getMultiaddrs().forEach((ma) => console.log(`Listening on: ${ma.toString()}`));
 }
